5cf39d314af02a45b9968319d3082586c6329e82
developer-256 <ch256.it@gmail.com>
2024-09-05 15:45:35 +0500

    first commit

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./hero.module.scss";
+const Hero = () => {
+  return (
+    <section className="border-[10px] border-red-700 w-screen h-screen">
+      <div></div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/Second.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./hero.module.scss";
+const Second = () => {
+  return (
+    <section className="border-[10px] border-yellow-700 w-screen h-screen">
+      <div></div>
+    </section>
+  );
+};
+
+export default Second;
